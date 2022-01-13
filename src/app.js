93fa92c808c053380422eed273500b2135b42c54
timurMikolenko <warfacemik33@gmail.com>
2022-01-13 22:43:46 +0500

    первый комит

---
+++ src/app.js
@@ -0,0 +1,50 @@
+var mysql = require('mysql');
+var express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log("сервер запущен "+PORT);
+});
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '',
+  database:  'wp_new',
+  port: "3306"
+});
+
+connection.connect(err => {
+    if(err){
+        console.log(err);
+        return err;
+    }else{
+        console.log('ok');
+    }
+});
+
+
+function getQur(callback){
+    let query = "SELECT posts_title, posts_text, posts_img FROM my_posts";
+    connection.query(query, 
+        (err, result, field) => {
+            // console.log(result);
+            callback(result);
+    });
+}
+
+// let query2 = "SELECT * FROM wp_users";
+//     connection.query(query2, 
+//         (err, result, field) => {
+//             console.log(err);
+//             console.log(result);
+//     });
+
+app.get("/api", (req, res)=>{
+    getQur((drive)=>{ 
+        res.json({posts_data: drive});
+        // console.log(drive);
+        // connection.end();
+     });
+});
